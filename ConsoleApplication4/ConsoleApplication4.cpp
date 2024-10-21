5e847c327929de28a255d6c63c6c60bfda0fd3c8
rampage <rampaga993@gmail.com>
2024-10-21 11:43:24 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int reverse_number(int n, int reversed = 0) {
+    // Базовый случай: если число стало нулем, возвращаем перевернутое число
+    if (n == 0) {
+        return reversed;
+    }
+
+    // Рекурсивный вызов: добавляем последнюю цифру к перевернутому числу
+    return reverse_number(n / 10, reversed * 10 + n % 10);
+}
+
+int main() {
+    int number = 321;
+    int reversed_number = reverse_number(number);
+    std::cout << reversed_number << std::endl;  // Вывод: 123
+    return 0;
+}
